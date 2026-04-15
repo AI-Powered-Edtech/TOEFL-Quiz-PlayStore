ba2cbde2aeaa2a3ce6c7fc1745d3f963791bfa50
juana <solusismart@gmail.com>
2026-04-16 01:52:55 +0700

    chore: remove temp files, QA screenshots, test scripts; update .gitignore

--- clear_cache.js
+++
@@ -1,3 +0,0 @@
-// Wait, I can't easily execute JS in the browser from the terminal unless I use Playwright script. 
-// Or I can just navigate to the app, log out via UI, or use browser_type. 
-// Let's just use the UI.
